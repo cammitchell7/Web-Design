614fee85338e8940dcf6c6c4233113f71fdfa2c0
Cameron Mitchell <camitch777@gmail.com>
2021-11-30 19:23:55 -0500

    opponent choice working

--- js/page.js
+++ js/page.js
@@ -2,13 +2,23 @@ function compChoice() {
   var rnd = Math.random();
   if(rnd < .334){
     rnd = 0;    // 0 will represent rock
+    document.getElementById('o-img').src = "svg/rock.svg";
   }
   else if (rnd > .334 && rnd < .667){
     rnd = 1;    // 1 will represent paper
+    document.getElementById('o-img').src = "svg/paper.svg";
   }
   else{
     rnd = 2;    // 2 will represent scissors
+    document.getElementById('o-img').src = "svg/scissors.svg";
+  }
+  return rnd;
+}
+
+function choice(player) {
+  var comp = compChoice();
+  if(player === comp){
+    document.getElementById('result').innerHTML = "It's a Tie!"
   }
 
-  document.getElementById('result').innerHTML = rnd;
 }
