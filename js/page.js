e0a3d6d6958f5c3e8f9415ee70d20966715d3b4f
Cameron Mitchell <camitch777@gmail.com>
2021-11-30 19:58:56 -0500

    finishing up the JS logic and adding in a tied game counter

--- js/page.js
+++ js/page.js
@@ -1,3 +1,7 @@
+var playerScore = 0;
+var compScore = 0;
+var ties = 0;
+
 function compChoice() {
   var rnd = Math.random();
   if(rnd < .334){
@@ -18,7 +22,44 @@ function compChoice() {
 function choice(player) {
   var comp = compChoice();
   if(player === comp){
-    document.getElementById('result').innerHTML = "It's a Tie!"
+    document.getElementById('result').innerHTML = "It's a Tie!";
+    ties += 1;
+    document.getElementById('tscore').innerHTML = ties;
   }
-
+  else if(player === 0){
+    if(comp === 1){
+      document.getElementById('result').innerHTML = "Paper Wins!";
+      compScore += 1;
+      document.getElementById('oscore').innerHTML = compScore;
+    }
+    else {
+      document.getElementById('result').innerHTML = "Rock Wins!";
+      playerScore += 1;
+      document.getElementById('pscore').innerHTML = playerScore;
+    }
+  }
+  else if(player === 1){
+    if(comp === 0){
+      document.getElementById('result').innerHTML = "Paper Wins!";
+      playerScore += 1;
+      document.getElementById('pscore').innerHTML = playerScore;
+    }
+    else {
+      document.getElementById('result').innerHTML = "Scissors Wins!";
+      compScore += 1;
+      document.getElementById('oscore').innerHTML = compScore;
+    }
+  }
+  else if(player === 2){
+    if(comp === 0){
+      document.getElementById('result').innerHTML = "Rock Wins!";
+      compScore += 1;
+      document.getElementById('oscore').innerHTML = compScore;
+    }
+    else {
+      document.getElementById('result').innerHTML = "Scissors Wins!";
+      playerScore += 1;
+      document.getElementById('pscore').innerHTML = playerScore;
+    }
+  }
 }
